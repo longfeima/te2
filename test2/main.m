07a36459b9b5d81b0115a58fa67258cafa25a61d
龙飞马 <longfeima@seven.local>
2018-04-25 16:48:28 +0800

    jhhhhhh

---
+++ test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by 龙飞马 on 2018/4/25.
+//  Copyright © 2018年 龙飞马. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
